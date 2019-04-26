7825274e286ec8ae17ea65578cab6674849970f1
AJAY SHARMA G S <gs.ajaysharma@gmail.com>
2019-04-26 19:01:52 +0530

    Half project

---
+++ Organic_Store/main.js
@@ -0,0 +1,119 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAJgTqulenhqg-w2NpnevLo4g1qg97qozg",
+    authDomain: "origano-1a760.firebaseapp.com",
+    databaseURL: "https://origano-1a760.firebaseio.com",
+    projectId: "origano-1a760",
+    storageBucket: "origano-1a760.appspot.com",
+    messagingSenderId: "150780361565"
+  };
+  firebase.initializeApp(config);
+  //GLOBAL
+  var products=[];
+  var cartItems=[];
+  var cart_n = document.getElementById('cart_n');
+  //DIVS
+  var fruitDIV= document.getElementById("fruitDIV");
+  var juiceDIV = document.getElementById("juiceDIV");
+  var saladDIV = document.getElementById("saladDIV");
+  //INFORMATION
+  var FRUIT=[
+      {name:"Apple" , price:10},
+      {name:"Orange" , price:10},
+      {name:"Cherry" , price:10},
+      {name:"Strawberry" , price:10},
+      {name:"Kiwi" , price:10},
+      {name:"Banana" , price:10}
+    ];
+  var JUICE=[
+      {name:"Juice #1" , price:100},
+      {name:"Juice #2" , price:110},
+      {name:"Juice #3" , price:120}
+    ];
+  var SALAD=[
+    {name:"Salad #1" , price:11},
+    {name:"Salad #2" , price:12},
+    {name:"Salad #3" , price:150} ];
+  //HTML
+function HTMLfruitProduct(con){
+    let URL = `img/fruits/fruit${con}.jpeg`;
+    let btn = `btnFruit${con}`;
+    return `
+       <div class="col-md-4">
+         <div class="card mb-4 shadow-sm">
+            <img class="card-img-top" style="height:16rem;" src="${URL}"
+            alt="Card image cap">
+            <div class="card-body">
+            <i style="color:orange;" class="fa fa-star" ></i>
+            <i style="color:orange;" class="fa fa-star" ></i>
+            <i style="color:orange;" class="fa fa-star" ></i>
+            <i style="color:orange;" class="fa fa-star" ></i>
+            <i style="color:orange;" class="fa fa-star" ></i>
+            <p class="card-text">${FRUIT[con].name}</p>
+            <p class=""card-text">Price: ${FRUIT[con].price}.00</p>
+            <div class="d-flex justify-content-between align-items-center">
+              <div class="btn-group">
+                 <button type="button" onclick="cart2('${FRUIT[con]
+                    .name}','${FRUIT[con].price}','${URL},'${con}','${btn}')" 
+                    class="btn btn-sm btn-outline-secondary" >Buy</button>
+
+                 <button id="${btn}" type="button" onclick="cart('${FRUIT[con].name}',
+                 '${FRUIT[con].price}','${URL}',${con}','${btn}')" class="btn btn-sm btn-outline-secondary" >Add to cart</button>
+               </div>
+               <small class="text-muted">Free Shipping </small>
+              </div>
+            
+            </div>
+         </div>
+
+       </div>
+    `
+}
+
+function HTMLjuiceProduct(con){
+    let URL = `img/juice/juice${con}.jpeg`;
+    let btn = `btnJuice${con}`;
+    return `
+       <div class="col-md-4">
+         <div class="card mb-4 shadow-sm">
+            <img class="card-img-top" style="height:16rem;" src="${URL}"
+            alt="Card image cap">
+            <div class="card-body">
+            <i style="color:orange;" class="fa fa-star" ></i>
+            <i style="color:orange;" class="fa fa-star" ></i>
+            <i style="color:orange;" class="fa fa-star" ></i>
+            <i style="color:orange;" class="fa fa-star" ></i>
+            <i style="color:orange;" class="fa fa-star" ></i>
+            <p class="card-text">${JUICE[con].name}</p>
+            <p class=""card-text">Price: ${JUICE[con].price}.00</p>
+            <div class="d-flex justify-content-between align-items-center">
+              <div class="btn-group">
+                 <button type="button" onclick="cart2('${JUICE[con]
+                    .name}','${JUICE[con].price}','${URL},'${con}','${btn}')" 
+                    class="btn btn-sm btn-outline-secondary" >Buy</button>
+
+                 <button id="${btn}" type="button" onclick="cart('${JUICE[con].name}',
+                 '${JUICE[con].price}','${URL}',${con}','${btn}')" class="btn btn-sm btn-outline-secondary" >Add to cart</button>
+               </div>
+               <small class="text-muted">Free Shipping </small>
+              </div>
+            
+            </div>
+         </div>
+
+       </div>
+    `
+}
+
+
+
+//RENDER
+function render(){
+   for(let index = 0; index <= 6; index++){
+       fruitDIV.innerHTML+=`${HTMLfruitProduct(index)}`;
+   }
+   for(let index = 0; index <= 3; index++){
+    juiceDIV.innerHTML+=`${HTMLjuiceProduct(index)}`;
+}
+}
